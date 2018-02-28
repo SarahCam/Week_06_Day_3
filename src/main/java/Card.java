fdcf3f13c4fe16eff32b880612fff85917561556
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-28 12:31:51 +0000

    Added some more descriptive comments to Card class. Added enumValue to ValueType enum, which is the value of the enum. Added SETTER and GETTER capabilities to ValueType enum, so the enumValue can be assigned to a private instance variable, and other classes can get the enumValue if needed.

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,21 +1,21 @@
 public class Card {
 
-    // Declare the following instance variables for the Card class:
+    // INSTANCE VARIABLES: Declare the following private instance variables for the Card class:
     private SuitType suit;
     private ValueType value;
 
-    // Create the constructor, which takes in a suit and value and assigns them to the Card class instance variables:
+    // SETTER: Create the constructor, which takes in a suit and value and assigns them to the Card class instance variables:
     public Card(SuitType suit, ValueType value){
         this.suit = suit;
         this.value = value;
     }
 
-    // Method to expose the private Card 'suit' property to other classes:
+    // GETTER: Method to expose the private Card 'suit' property to other classes:
     public SuitType getSuit(){
         return this.suit;
     }
 
-    // Method to expose the private Card 'value' property to other classes:
+    // GETTER: Method to expose the private Card 'value' property to other classes:
     public ValueType getValue(){
         return this.value;
     }
--- src/main/java/ValueType.java
+++ src/main/java/ValueType.java
@@ -1,15 +1,30 @@
 public enum ValueType {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+    ACE(1),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);       // Don't forget the semi-colon!
+
+    // INSTANCE VARIABLE: Declare a private instance variable called enumValue, which is 'final', i.e cannot be changed:
+    private final int enumValue;
+
+    // SETTER: Create the constructor, which:
+    // - Takes in the enumValue from the above enumKey(enumValue) pairs
+    // - Assigns it to the declared private instance variable: enumValue
+    ValueType(int enumValue){
+        this.enumValue = enumValue;
+    }
+
+    // GETTER: Method to expose the private ValueType 'enumValue' property to other classes:
+    public int getEnumValue() {
+        return this.enumValue;
+    }
 }
