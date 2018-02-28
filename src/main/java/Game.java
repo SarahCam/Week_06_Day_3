f8a9e920874acb0519ad170153bd521d338a8a6a
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-28 18:17:23 +0000

    Added a new Game and GameTest classes.

---
+++ src/main/java/Game.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Game {
+
+    // INSTANCE VARIABLES:
+    private ArrayList<Player> players;
+    private Deck deck;
+
+    //SETTER: CONSTRUCTOR:
+    public Game(){
+        players = new ArrayList<Player>();
+        deck = new Deck();
+    }
+
+}
