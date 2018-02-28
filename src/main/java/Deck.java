d42eac2a749cc0ea9855b202cee190012d0a38a7
Sarah Campbell <scampbell.home@btinternet.com>
2018-02-28 16:42:31 +0000

    Added a new method: populate() to the Deck class, with a couple of TDD tests to validate (1) that 52 cards are created, and (2) that the cards are different.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -10,8 +10,22 @@ public class Deck {
         this.cards = new ArrayList<Card>();
     }
 
+    // GETTER: Method to expose the private Deck 'cards' property to other classes:
     public ArrayList<Card> getCards(){
         return this.cards;
+    }
+
+    // Method to populate our empty deck of cards with all 52 playing cards:
+    // For each suit in our list of SuitType values, i.e. HEARTS, DIAMONDS, SPADES, CLUBS
+    // And then for each value in our list of ValueType values, i.e. ACE, TWO, THREE,...KING
+    // Add a new card with that suit and that value to the Deck's cards ArrayList:
+    public void populate(){
+        for(SuitType suit : SuitType.values()){
+            for(ValueType value : ValueType.values()){
+                Card card = new Card(suit, value);
+                this.cards.add(card);
+            }
+        }
     }
 
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -16,4 +16,22 @@ public class DeckTest {
     public void canGetCards(){
         assertEquals(0, deck.getCards().size());
     }
+
+    @Test
+    public void canPopulate___HAVE_52_CARDS(){
+        deck.populate();
+        assertEquals(52, deck.getCards().size());
+    }
+
+    @Test
+    public void canPopulate___HAVE_EACH_KIND_OF_CARD_IN_DECK___SAMPLE_TEST(){
+        deck.populate();
+        Card firstCard = deck.getCards().get(0);
+        assertEquals(SuitType.HEARTS, firstCard.getSuit());
+        assertEquals(ValueType.ACE, firstCard.getValue());
+        Card lastCard = deck.getCards().get(51);
+        assertEquals(SuitType.CLUBS, lastCard.getSuit());
+        assertEquals(ValueType.KING, lastCard.getValue());
+    }
+
 }
